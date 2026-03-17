3407cbf8fcc9ee22f2d88c99dd95683becc3522e
alamgirkamal <alamgir.kamal@gmail.com>
2026-03-17 01:58:37 +0000

    Add commands to kill user processes

--- shell/command-line-calls.sh
+++ shell/command-line-calls.sh
@@ -3,3 +3,7 @@ sudo userdel <user-name>
 
 # Delete user including home folder and mail spool
 sudo userdel -r <user-name>
+
+# Kill all processes running by user
+sudo killall -u <user-name>
+sudo pkill -KILL -u <user-name>
